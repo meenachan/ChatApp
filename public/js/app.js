d23dca843fdc78407d4c4af3e0db355672c80eb5
meenachan <mcquitypie3@gmail.com>
2020-05-24 22:09:32 +0530

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+const socket = io('http://localhost:3000')
+const messageForm = document.getElementById('send-container')
+const messageContainer = document.getElementById('message-container')
+const messageInput = document.getElementById('message')
+
+var name;
+
+function show(){  
+    document.getElementById('index').style.display="none";
+document.getElementById('chat').style.display="block";
+var  namein = document.getElementById('user-name').value;
+  name = namein;
+  //  const name = prompt('Whats your name?')
+appendMessage("You Joined")
+socket.emit('new-user',name)
+
+socket.on('chat-message', data=>{
+appendMessage(`${data.name} :${data.message}`)
+})
+socket.on('user-connected', name=>{
+appendMessage(`${name} connected`)
+})
+socket.on('user-disconnected', name=>{
+appendMessage(`${name} disconnected`)
+})
+messageForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You :${message}`)
+    socket.emit('send-chat-message',message)
+    messageInput.value=''
+})
+}
+
+function appendMessage(message){
+const messageElement = document.createElement('div')
+messageElement.innerText=message
+messageContainer.append(messageElement)
+}
